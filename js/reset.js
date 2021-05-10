956df37d1ebbfa9af27738c1af145255bb6fa16e
Bruno Oliveira <brunosilvadeoliveira.10@gmail.com>
2021-05-10 17:19:51 -0300

    Resetando estilos

---
+++ js/reset.js
@@ -0,0 +1,22 @@
+function Reset(){
+    gamer_one["jogadas"] = []
+    gamer_two["jogadas"] = []
+    one["caminho"].style.backgroundColor = '#FFF'
+    two["caminho"].style.backgroundColor = '#FFF'
+    three["caminho"].style.backgroundColor = '#FFF'
+    four["caminho"].style.backgroundColor = '#FFF'
+    five["caminho"].style.backgroundColor = '#FFF'
+    six["caminho"].style.backgroundColor = '#FFF'
+    seven["caminho"].style.backgroundColor = '#FFF'
+    eight["caminho"].style.backgroundColor = '#FFF'
+    nine["caminho"].style.backgroundColor = '#FFF'
+    one["click"] = false
+    two["click"] = false
+    three["click"] = false
+    four["click"] = false
+    five["click"] = false
+    six["click"] = false
+    seven["click"] = false
+    eight["click"] = false
+    nine["click"] = false
+}

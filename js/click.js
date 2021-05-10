25613f686e1be1db65361ea12ad2419f835991ce
Bruno Oliveira <brunosilvadeoliveira.10@gmail.com>
2021-05-10 14:29:14 -0300

    Efeito de click nas posiçoes

---
+++ js/click.js
@@ -0,0 +1,44 @@
+function Clicked(pos, gamer){
+    if(gamer % 2 == 0){
+        pos.style.backgroundColor = '#00F'
+    }else{
+        pos.style.backgroundColor = '#F00'
+    }
+}
+var vez = 0;
+one.onclick = function(){
+    Clicked(one, vez)
+    vez++
+}
+two.onclick = function(){
+    Clicked(two, vez)
+    vez++
+}
+three.onclick = function(){
+    Clicked(three, vez)
+    vez++
+}
+four.onclick = function(){
+    Clicked(four, vez)
+    vez++
+}
+five.onclick = function(){
+    Clicked(five, vez)
+    vez++
+}
+six.onclick = function(){
+    Clicked(six, vez)
+    vez++
+}
+seven.onclick = function(){
+    Clicked(seven, vez)
+    vez++
+}
+eight.onclick = function(){
+    Clicked(eight, vez)
+    vez++
+}
+nine.onclick = function(){
+    Clicked(nine, vez)
+    vez++
+}
